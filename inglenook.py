ccaaaf3316c1090c5440f5aa33151de5910b6507
Emlyn Price <emlyn.je.price@gmail.com>
2018-12-23 16:06:59 +0000

    Code tidying, rename sidings to be more general

--- inglenook.py
+++ inglenook.py
@@ -12,11 +12,13 @@ def get_args():
 
     parser_322 = subparsers.add_parser('322',
                                        help='Generate for a 322 inglenook')
+
     parser_322.add_argument('wagons', nargs=6)
     parser_322.set_defaults(func=get_starting_setup_322)
 
     parser_533 = subparsers.add_parser('533',
                                        help='Generate for a 533 inglenook')
+
     parser_533.add_argument('wagons', nargs=8)
     parser_533.set_defaults(func=get_starting_setup_533)
 
@@ -26,34 +28,37 @@ def get_args():
 def get_starting_setup_322(args):
     """Generate the starting positions of the wagons."""
 
+    # Copy the wagons list for convenience
     wagons = args.wagons
-    # First decide where the wagons will go
-    num_back = randint(2, 3)
 
-    if num_back == 2:
-        num_mid = 2
-        num_front = 2
+    # The long siding can contain either 2 or 3 wagons.
+    num_long = randint(2, 3)
 
-    if num_back == 3:
+    if num_long == 2:
+        num_short_1 = 2
+        num_short_2 = 2
+
+    if num_long == 3:
         fill_mid = choice([True, False])
         if fill_mid:
-            num_mid = 2
-            num_front = 1
+            num_short_1 = 2
+            num_short_2 = 1
         if not fill_mid:
-            num_mid = 1
-            num_front = 2
+            num_short_1 = 1
+            num_short_2 = 2
 
     # Randomise the order of the wagons
     shuffle(wagons)
 
-    back = wagons[0:num_back]
-    mid = wagons[num_back:num_back+num_mid]
-    front = wagons[num_back+num_mid:]
+    # Slice the shuffled list to get the wagons to go in each siding
+    long_siding = wagons[0:num_long]
+    short_1 = wagons[num_long:num_long+num_short_1]
+    short_2 = wagons[num_long+num_short_1:]
 
     print("The starting setup is:")
-    print("Back: " + str(back))
-    print("Middle: " + str(mid))
-    print("Front: " + str(front))
+    print("Long siding:         " + str(long_siding))
+    print("First short siding:  " + str(short_1))
+    print("Second short siding: " + str(short_2))
 
 def get_starting_setup_533(args):
     """Generate the starting positions of the wagons."""
@@ -61,16 +66,16 @@ def get_starting_setup_533(args):
     wagons = args.wagons
     # First decide where the wagons will go
     # The longest siding must start with between 2 and 5 wagons
-    num_back = randint(2, 5)
+    num_long = randint(2, 5)
 
     # If there are two in the long siding, both the short sidings are full
-    if num_back == 2:
-        num_mid = 3
-        num_front = 3
+    if num_long == 2:
+        num_short_1 = 3
+        num_short_2 = 3
 
     # If there are three or more wagons in the long siding, then the total
-    # number in the short sidings is 8 - num_back
-    if num_back >= 3:
+    # number in the short sidings is 8 - num_long
+    if num_long >= 3:
         # If there are five wagons in the long siding, mid can have 0 to
         # 3 wagons: minimum of 8 - (num_long + 3)
         # If there are four in the long siding, mid must have at least one
@@ -78,22 +83,22 @@ def get_starting_setup_533(args):
         # If there are three in the long siding, mid must have at least two in:
         # 8 - (num_long + 3)
 
-        min_short = 8 - (num_back + 3)
-        num_mid = randint(min_short, 3)
-        num_front = 8 - num_back - num_mid
-
+        min_short = 8 - (num_long + 3)
+        num_short_1 = randint(min_short, 3)
+        num_short_2 = 8 - num_long - num_short_1
 
     # Randomise the order of the wagons
     shuffle(wagons)
 
-    back = wagons[0:num_back]
-    mid = wagons[num_back:num_back+num_mid]
-    front = wagons[num_back+num_mid:]
+    # Slice the shuffled list to get the wagons to go in each siding
+    long_siding = wagons[0: num_long]
+    short_1 = wagons[num_long: num_long+num_short_1]
+    short_2 = wagons[num_long+num_short_1: ]
 
     print("The starting setup is:")
-    print("Back: " + str(back))
-    print("Middle: " + str(mid))
-    print("Front: " + str(front))
+    print("Long siding:         " + str(long_siding))
+    print("First short siding:  " + str(short_1))
+    print("Second short siding: " + str(short_2))
 
 def get_target(wagons, inglenook_type):
     """Set the target train to be assembled. Length depends on the type of
